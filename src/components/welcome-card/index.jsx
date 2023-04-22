9d5a4e6e34d6423c796c9aeb2d1761a5d542c9df
Aleksei <alikbezmoshyk@gmail.com>
2023-04-22 23:55:11 +0300

    - удален закоментированный код

--- src/components/welcome-card/index.jsx
+++ src/components/welcome-card/index.jsx
@@ -1,21 +1,3 @@
-// import './styles.css';
-// import { Button, Card, CardContent, CardHeader, Container } from '@mui/material';
-// import { useContext } from 'react';
-// import { PostsContext } from '../../contexts/post-context';
-
-// export const WelcomeCard = () => {
-
-//   const { createPost } = useContext(PostsContext);
-
-//   return <Container maxWidth={'lg'} sx={{marginTop: '20px'}}>
-//     <Card>
-//       <CardHeader title={'Добро пожаловать'}/>
-//       <CardContent>
-//         <Button onClick={createPost}>Создать пост</Button>
-//       </CardContent>
-//     </Card>
-//   </Container>;
-// };
 import './styles.css';
 import { Button, Card, CardContent, CardHeader, Container } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
